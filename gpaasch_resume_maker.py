906b73975b08c2791df1c5aea2a6251156b93f0d
Graham Paasch <grahampaasch@gmail.com>
2020-05-18 10:43:07 -0500

    addressing ipython import issue

--- gpaasch_resume_maker.py
+++ gpaasch_resume_maker.py
@@ -1,6 +1,6 @@
+# use "python -m IPython" for testing
 import json
-#from docx import Document
-#from docx.shared import Inches
+import docx
 
 with open('gpaasch_2020_resume.json') as f:
     resume = json.loads(f.read())
