af5e3502317f15d146938e214b3abf932f6c1786
Graham Paasch <grahampaasch@gmail.com>
2020-05-18 10:58:04 -0500

    hello world stype inital proof of concept

--- gpaasch_resume_maker.py
+++ gpaasch_resume_maker.py
@@ -5,3 +5,8 @@ import docx
 with open('gpaasch_2020_resume.json') as f:
     resume = json.loads(f.read())
 
+document = docx.Document()
+
+document.add_heading(resume['basics']['name'], 0)
+
+document.save('gpaasch_2020_resume.docx')
