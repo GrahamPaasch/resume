13bb5ea30ced3a5020a40c3b62715a6ce4f08af2
Graham Paasch <grahampaasch@gmail.com>
2020-12-01 21:07:38 -0600

    returning to this months later... looks like there were some changes in the .py file that I did not commit that I have forgotten writing, this update is to add my current job experience to my resume, but I am committing those changes as well

--- gpaasch_resume_maker.py
+++ gpaasch_resume_maker.py
@@ -5,24 +5,31 @@ import docx
 with open('gpaasch_2020_resume.json') as f:
     resume = json.loads(f.read())
 
+length_in_points = docx.shared.Pt
 document = docx.Document()
 align_center = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+align_left = docx.enum.text.WD_ALIGN_PARAGRAPH.LEFT
 
-my_text = document.add_paragraph()
-my_text.alignment = align_center
-my_name_title = my_text.add_run(resume['basics']['name'] + '\n', 'Title Char')
+email_phone_website_address = \
+    resume['basics']['email'] + ' | ' + \
+    resume['basics']['phone'] + ' | ' + \
+    resume['basics']['website'] + ' | ' + \
+    resume['basics']['location']['address'] + '\n'
 
-my_text.add_run(
-    resume['basics']['email']
-    + ' | '
-    + resume['basics']['phone']
-    + ' | '
-    + resume['basics']['website']
-    + ' | '
-    + resume['basics']['location']['address']
-    + '\n'
-)
+length_of_dashes = len(email_phone_website_address) - 1
 
-my_text.add_run('--------------------------------------------------------------------------\n')
+contact_info = document.add_paragraph()
+contact_info.alignment = align_center
+contact_info.add_run(resume['basics']['name'] + '\n', 'Title Char')
+contact_info.add_run(email_phone_website_address)
+contact_info.add_run('- ' * length_of_dashes)
+contact_info.paragraph_format.space_after = length_in_points(0)
+
+certification = document.add_paragraph()
+certification.alignment = align_left
+certification.add_run(resume['awards'][0]['title'] + '\n')
+certification.add_run(resume['awards'][1]['title'] + '\n')
+certification.add_run('- ' * length_of_dashes)
+certification.paragraph_format.space_before = length_in_points(0)
 
 document.save('gpaasch_2020_resume.docx')
